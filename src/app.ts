ffb5d62fb0612bff6c44c99077b025a8b28d1730
Kashif-79 <kashif.r.reza@gmail.com>
2025-11-30 19:52:47 +0600

    commit+user modules

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { userRoutes } from "./app/modules/user/user.routes";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "ARA-Healthcare Server",
+  });
+});
+
+app.use("/api/v1/user", userRoutes);
+
+export default app;
